c589075cca89bc933b80041aeab50d6b395767d4
John <user@John-Pooleys-Macbook.local>
2020-01-28 09:34:44 +0000

    describes who won and with what hand they won with

--- models/rock_paper_scissors.rb
+++ models/rock_paper_scissors.rb
@@ -3,19 +3,19 @@ class Rock_paper_scissors
   def self.hand_game(hand1, hand2)
     return "It's a draw!" unless hand1 != hand2
     if hand1 == "rock"
-      return "player 1 wins!" unless hand2 == "paper"
-      return "player 2 wins!"
+      return "player 1 wins using #{hand1}! Player 2 lost with #{hand2}" unless hand2 == "paper"
+      return "player 2 wins using #{hand2}! Player 1 lost with #{hand1}"
     elsif hand1 == "paper"
-      return "player 1 wins!" unless hand2 == "scissors"
-      return "player 2 wins!"
+      return "player 1 wins using #{hand1}! Player 2 lost with #{hand2}player 1 wins using #{hand1}! Player 2 lost with #{hand2}" unless hand2 == "scissors"
+      return "player 2 wins using #{hand2}! Player 1 lost with #{hand1}"
     elsif hand1 == "scissors"
-      return "player 1 wins!" unless hand2 == "rock"
-      return "player 2 wins!"
+      return "player 1 wins using #{hand1}! Player 2 lost with #{hand2}" unless hand2 == "rock"
+      return "player 2 wins using #{hand2}! Player 1 lost with #{hand1}"
     else
       return "try again!"
     end
 
   end
 
-  
+
 end
